8af504f39848530c98a3dea502b4637129f09320
SquareBurst35 <catitoggripe@gmail.com>
2026-09-03 23:13:45 -0300

    chore: atualizar cache da progressão

--- progression.js
+++ progression.js
@@ -1,4 +1,4 @@
-import { characterLevel, usesSeparateLevel } from "./rules.js?v=6";
+import { characterLevel, usesSeparateLevel } from "./rules.js?v=7";
 
 export const LEVEL_CAP = 20;
 export const CLASS_POWER_LEVELS = [3, 6, 9, 12, 15, 18];
